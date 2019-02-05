a59df91c06876c124d86ff1cd3a04df8b60b58e8
mohubetroh <45729485+mohubetroh@users.noreply.github.com>
2019-02-05 20:22:58 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,17 +1,19 @@
 var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate(){
+var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( password == "postsenkgo"){
-alert ("Login successfully GENG");
-window.location = "login.html"; // Redirecting to other page.
+if ( username == "hog" && password == "hog"){
+alert ("Login successfully");
+window.location = "home.html"; // Redirecting to other page.
 return false;
 }
 else{
 attempt --;// Decrementing by one.
-alert("salah Password "+attempt+" UPAYA MASUK");
+alert("You have left "+attempt+" attempt;");
 // Disabling fields after 3 attempts.
 if( attempt == 0){
+document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
 document.getElementById("submit").disabled = true;
 return false;
